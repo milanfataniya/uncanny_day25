174187e525d3848727f8df9c9c97fddbe01e2a31
milan <milanfataniya2021@gmail.com>
2026-06-10 10:45:44 +0530

    new feture

---
+++ lib/presantation/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Milan")
+          ],
+        ),
+      )
+    );
+  }
+}
