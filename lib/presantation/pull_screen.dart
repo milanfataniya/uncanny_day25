c6f72de22de8f413a015591e631e1c1a2ceea3e4
milan <milanfataniya2021@gmail.com>
2026-06-10 12:50:39 +0530

    dev1 puished

---
+++ lib/presantation/pull_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class pull_screen extends StatelessWidget {
+  const pull_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Pull to Refresh')));
+  }
+}
